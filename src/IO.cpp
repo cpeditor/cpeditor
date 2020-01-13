f1add1dfbb85689a41502331c72aaa2cd696144e
Ashar Khan <ashar786khan@gmail.com>
2020-01-13 13:31:23 +0530

    fix license header text

--- src/IO.cpp
+++ src/IO.cpp
@@ -1,19 +1,20 @@
 /*
- * Copyright (C) 2019 Ashar Khan <ashar786khan@gmail.com>
- *
- * This file is part of CPEditor.
- *
- * CPEditor is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * I will not be responsible if CPEditor behaves in unexpected way and
- * causes your ratings to go down and or loose any important contest.
- *
- * Believe Software is "Software" and it isn't not immune to bugs.
- *
- */
+* Copyright (C) 2019 Ashar Khan <ashar786khan@gmail.com> 
+* 
+* This file is part of CPEditor.
+*  
+* CPEditor is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* I will not be responsible if CPEditor behaves in unexpected way and
+* causes your ratings to go down and or loose any important contest.
+* 
+* Believe Software is "Software" and it isn't immune to bugs.
+* 
+*/
+
 
 #include <IO.hpp>
 
