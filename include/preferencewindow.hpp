c7b1567c7139380d534e42bc11b843e77fb660f8
Darren Li <wtnmmo@gmail.com>
2020-01-31 20:43:35 +0800

    separation

--- include/preferencewindow.hpp
+++ include/preferencewindow.hpp
@@ -43,39 +43,22 @@ class PreferenceWindow : public QMainWindow
   private slots:
 
     void on_ok_clicked();
-
     void on_cancel_clicked();
-
     void on_apply_clicked();
-
     void on_hotkeys_clicked(bool checked);
-
     void on_font_button_clicked();
-
     void on_cpp_template_clicked();
-
     void on_py_template_clicked();
-
     void on_java_template_clicked();
-
     void on_load_snippets_from_files_clicked();
-
     void on_extract_snippets_to_files_clicked();
-
     void onSnippetsLangChanged(const QString &lang);
-
     void onCurrentSnippetChanged(const QString &text);
-
     void applySettingsToEditor();
-
     void on_snippet_save_clicked();
-
     void on_snippet_new_clicked();
-
     void on_snippet_delete_clicked();
-
     void on_snippet_rename_clicked();
-
     void on_transparency_slider_valueChanged(int value);
 
   private:
