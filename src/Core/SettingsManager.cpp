6436e41dfcb828e9ce151666cfac427cc3505ddd
ouuan <y___o___u@126.com>
2020-02-06 18:25:28 +0800

    feat: Use monospace as default font; monospace only in preferences
    1. Use monospace as the default font.
    
    2. Now only monospace fonts will be displayed in the font selection
       window.
    
    This suggestion is from @Arpit in Telegram group.
    
    > One suggestion please keep Monospace or any other font which provides
    > good indentation. Even in my college many students deleted your
    > editorjust because of that formatting problem because of default Sans
    > Serif font.

--- src/Core/SettingsManager.cpp
+++ src/Core/SettingsManager.cpp
@@ -160,7 +160,7 @@ QString SettingManager::getTemplatePathPython()
 }
 QString SettingManager::getFont()
 {
-    return mSettings->value("font", "").toString();
+    return mSettings->value("font", "monospace").toString();
 }
 
 QRect SettingManager::getGeometry()
