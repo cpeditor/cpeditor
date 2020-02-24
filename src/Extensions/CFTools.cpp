a8af71da7f9fd50b721722b4a8de0e590b8b9399
ouuan <y___o___u@126.com>
2020-02-24 21:56:08 +0800

    fix(CF Tool): Fix #179 color of Pretests Passed

--- src/Extensions/CFTools.cpp
+++ src/Extensions/CFTools.cpp
@@ -115,7 +115,8 @@ void CFTools::onReadReady()
     QString newResponse = CFToolProcess->readAll();
     Core::Log::i("cftools/onReadReady") << "\n" << newResponse << endl;
     auto shortStatus = newResponse.right(newResponse.size() - newResponse.indexOf("status:") - 8);
-    if (newResponse.contains("status: Happy New Year") || newResponse.contains("status: Accepted"))
+    if (newResponse.contains("status: Happy New Year") || newResponse.contains("status: Accepted") ||
+        newResponse.contains("status: Pretests Passed"))
     {
         Core::Log::i("cftools/onReadReady") << "solution was accepted " << shortStatus << endl;
         log->message("CF Tool", shortStatus, "green");
