9aa428efd8979172d5c145733263639bed87e677
ouuan <y___o___u@126.com>
2020-02-26 20:33:43 +0800

    chore: Fix grammar in comments

--- include/Core/Checker.hpp
+++ include/Core/Checker.hpp
@@ -9,7 +9,7 @@
  * (at your option) any later version.
  *
  * I will not be responsible if CPEditor behaves in unexpected way and
- * causes your ratings to go down and or loose any important contest.
+ * causes your ratings to go down and or lose any important contest.
  *
  * Believe Software is "Software" and it isn't immune to bugs.
  *
