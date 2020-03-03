2314157c7686e223d05a47516c90860d9b021bc4
ouuan <y___o___u@126.com>
2020-03-03 15:02:36 +0800

    chore: Change cpeditor in the comments to CP Editor
    "CPEditor" was replaced by "cpeditor", the comments are changed, too.
    
    But only the executable files and file paths should be "cpeditor",
    "CP Editor" in the comments shouldn't.

--- include/Widgets/TestCase.hpp
+++ include/Widgets/TestCase.hpp
@@ -1,14 +1,14 @@
 /*
  * Copyright (C) 2019-2020 Ashar Khan <ashar786khan@gmail.com>
  *
- * This file is part of cpeditor.
+ * This file is part of CP Editor.
  *
- * cpeditor is free software: you can redistribute it and/or modify
+ * CP Editor is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * I will not be responsible if cpeditor behaves in unexpected way and
+ * I will not be responsible if CP Editor behaves in unexpected way and
  * causes your ratings to go down and or lose any important contest.
  *
  * Believe Software is "Software" and it isn't immune to bugs.
