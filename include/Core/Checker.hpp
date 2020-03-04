123fa307111c67c09cd5be6b9f3d19648ef78074
ouuan <y___o___u@126.com>
2020-03-04 21:41:53 +0800

    docs(Checker): Add comments and minor changes

--- include/Core/Checker.hpp
+++ include/Core/Checker.hpp
@@ -15,6 +15,16 @@
  *
  */
 
+/*
+ * The checker is used to check whether an output is accepted.
+ * You can send the actual output and the expected output to a checker,
+ * and the checker will tell you whether this output is accepted or not.
+ * The checker should be setup before required to check outputs, and the
+ * response is not always immediate.
+ * The official testlib checkers are saved in the Qt Resources, and are
+ * compiled during the runtime.
+ */
+
 #ifndef CHECKER_HPP
 #define CHECKER_HPP
 
@@ -31,65 +41,172 @@ class Checker : public QObject
     Q_OBJECT
 
   public:
+    // The type of a checker
     enum CheckerType
     {
-        IgnoreTrailingSpaces,
-        Strict,
-        Ncmp,
-        Rcmp4,
-        Rcmp6,
-        Rcmp9,
-        Wcmp,
-        Nyesno,
-        Custom
+        /* built-in checkers */
+        IgnoreTrailingSpaces, // Ignore blank characters at the end of lines and blank lines at the end.
+        Strict,               // White space differences matters, except the differences between \n, \r and \r\n
+        /* testlib checkers */
+        Ncmp,   // ncmp.cpp in testlib, compare ordered sequences of signed int64 numbers
+        Rcmp4,  // rcmp4.cpp in testlib, compare two sequences of doubles, max absolute or relative error = 1e-4
+        Rcmp6,  // rcmp6.cpp in testlib, compare two sequences of doubles, max absolute or relative error = 1e-6
+        Rcmp9,  // rcmp9.cpp in testlib, compare two sequences of doubles, max absolute or relative error = 1e-9
+        Wcmp,   // wcmp.cpp in testlib, compare sequences of tokens
+        Nyesno, // nyesno.cpp in testlib, multiple YES/NO (case insensetive)
+        /* custom checkers */
+        Custom // a local custom checker chose by the user
     };
 
+    // The result of a check
     enum Verdict
     {
-        AC,
-        WA,
-        UNKNOWN
+        AC,     // Accepted
+        WA,     // Wrong answer
+        UNKNOWN // Used when the checker hasn't finished checking, or the checker failed
+                // Usually the checker doesn't return UNKNOWN, the verdict is set to UNKNOWN before the check begins
     };
 
-    Checker(CheckerType type, MessageLogger *logger, int timeLimit, QObject *parent);
-    Checker(const QString &path, MessageLogger *logger, int timeLimit, QObject *parent);
+    /*
+     * @brief construct a checker
+     * @param type the type of the checker
+     * @param logger the message logger that receives the messages
+     * @param timeLimit maximum time for a testlib checker to run
+     * @param parent the parent of a QObject
+     * @note Don't construct a custom checker by this.
+     */
+    Checker(CheckerType type, MessageLogger *logger, int timeLimit, QObject *parent = nullptr);
+
+    /*
+     * @brief construct a custom checker
+     * @param path the file path to the custom checker
+     * @param logger the message logger that receives the messages
+     * @param timeLimit maximum time for a testlib checker to run
+     * @param parent the parent of a QObject
+     */
+    Checker(const QString &path, MessageLogger *logger, int timeLimit, QObject *parent = nullptr);
+
+    /*
+     * @brief destruct a checker
+     * @note Tt kills the running compiler and checker, removes the temporary directory.
+     */
     ~Checker();
-    void prepare(const QString &command);
+
+    /*
+     * @brief prepare for checking
+     * @param compileCommand the command used to compile the checker
+     * @note Testlib checkers will be compiled after calling this function. This should be called only once.
+     */
+    void prepare(const QString &compileCommand);
+
+    /*
+     * @brief request the checker to check a testcase
+     * @param index the index of this testcase, used in messages and the result signals
+     * @param input the input of the testcase, not used in the built-in checkers
+     * @param output the output to check
+     * @param expected the expected output of the testcase
+     * @note This function doesn't return anything, it request the checker to check,
+     *       and the checker emits a signal when it's done
+     */
     void reqeustCheck(int index, const QString &input, const QString &output, const QString &expected);
 
   signals:
+    /*
+     * @brief return the check result
+     * @param index the index of the checked testcase
+     * @param verdict the result of this check
+     */
     void checkFinished(int index, Verdict verdict);
 
   private slots:
+    /*
+     * @brief the checker is compiled successfully
+     */
     void onCompilationFinished();
+
+    /*
+     * @brief failed to compile the checker
+     * @param error the error message provided by Core::Compiler
+     */
     void onCompilationErrorOccured(const QString &error);
+
+    /*
+     * @brief the compile process is killed
+     */
     void onCompilationKilled();
 
-    void onRunFinished(int index, const QString &out, const QString &err, int exitCode, int timeUsed);
+    /*
+     * @brief the checker process is finished
+     * @param index the index of the testcase
+     * @param out the stdout of the checker process
+     * @param err the stderr of the checker process
+     * @param exitCode the exit code of the checker process
+     */
+    void onRunFinished(int index, const QString &out, const QString &err, int exitCode);
+
+    /*
+     * @brief the checker failed to start
+     * @param index the index of the testcase
+     * @param error the error message provided by Core::Runner
+     */
     void onRunErrorOccured(int index, const QString &error);
+
+    /*
+     * @brief the checker hasn't finished in the time limit
+     * @param index the index of the testcase
+     */
     void onRunTimeout(int index);
+
+    /*
+     * @brief the checker process is killed
+     * @param index the index of the testcase
+     */
     void onRunKilled(int index);
 
   private:
+    /*
+     * @brief check the output against the expected output in IgnoreTrailingSpaces mode
+     * @param output the output to check
+     * @param expected the expected output to check the output against
+     * @return whether this output is accepted or not
+     */
     bool checkIgnoreTrailingSpaces(const QString &output, const QString &expected);
+
+    /*
+     * @brief check the output against the expected output in Strict mode
+     * @param output the output to check
+     * @param expected the expected output to check the output against
+     * @return whether this output is accepted or not
+     */
     bool checkStrict(const QString &output, const QString &expected);
+
+    /*
+     * @brief check a testcase
+     * @param input the input of the testcase
+     * @param output the output to check
+     * @param expected the expected output of the testcase
+     * @note this should only be called when the checker is compiled
+     */
     void check(int index, const QString &input, const QString &output, const QString &expected);
 
+    // a struct with the info of a testcase, or called a check task, used to save check requests
     struct Task
     {
         int index;
         QString input, output, expected;
     };
 
-    CheckerType checkerType;
-    QString checkerPath;
-    int timeLimit;
-    QTemporaryDir *tmpDir = nullptr;
-    MessageLogger *log = nullptr;
-    Compiler *compiler = nullptr;
-    QVector<Runner *> runner;
-    QVector<Task> pendingTasks;
-    bool compiled = false;
+    CheckerType checkerType;         // the type of the checker
+    QString checkerPath;             // the file path to the custom checker
+    int timeLimit;                   // the maximum time for a testlib checker to run
+    QTemporaryDir *tmpDir = nullptr; // the temp directory to save the I/O files, testlib.h and the compiled checker
+                                     // It's not needed by built-in checkers
+    MessageLogger *log = nullptr;    // the message logger to show messages to the user
+    Compiler *compiler = nullptr;    // the compiler used to compile the checker
+    QVector<Runner *> runner;        // the runners used to run the check processes
+    QVector<Task> pendingTasks;      // the unsolved check requests
+    bool compiled = false;           // whether the testlib checker is compiled or not
+                                     // It should be true for built-in checkers.
 };
 
 } // namespace Core
--- src/Core/Checker.cpp
+++ src/Core/Checker.cpp
@@ -48,8 +48,9 @@ Checker::~Checker()
         delete tmpDir;
 }
 
-void Checker::prepare(const QString &command)
+void Checker::prepare(const QString &compileCommand)
 {
+    // clear everything
     for (auto &t : runner)
         delete t;
     if (compiler)
@@ -59,13 +60,16 @@ void Checker::prepare(const QString &command)
 
     if (!compiled)
     {
+        // compile the checker if it's not compiled
+
         QString checkerResource;
 
+        // get the checker resource
         switch (checkerType)
         {
         case IgnoreTrailingSpaces:
         case Strict:
-            onCompilationFinished();
+            onCompilationFinished(); // terminate compilation if this is a built-in checker
             return;
         case Ncmp:
             checkerResource = ":/testlib/checkers/ncmp.cpp";
@@ -90,6 +94,7 @@ void Checker::prepare(const QString &command)
             break;
         }
 
+        // get the code of the checker
         QFile checkerResourceReader(checkerResource);
         if (!checkerResourceReader.open(QIODevice::ReadOnly | QIODevice::Text))
         {
@@ -98,6 +103,7 @@ void Checker::prepare(const QString &command)
         }
         QString checkerCode = checkerResourceReader.readAll();
 
+        // create a temporary directory
         tmpDir = new QTemporaryDir();
         if (!tmpDir->isValid())
         {
@@ -105,10 +111,12 @@ void Checker::prepare(const QString &command)
             return;
         }
 
+        // save the checker source file on the disk
         checkerPath = tmpDir->filePath("checker.cpp");
         if (!Util::saveFile(checkerPath, checkerCode, "Checker", false, log))
             return;
 
+        // save testlib.h on the disk
         QFile testlibReader(":/testlib/testlib.h");
         if (!testlibReader.open(QIODevice::ReadOnly | QIODevice::Text))
         {
@@ -119,29 +127,30 @@ void Checker::prepare(const QString &command)
         if (!Util::saveFile(tmpDir->filePath("testlib.h"), testlib_h, "Checker", false, log))
             return;
 
+        // start the compilation of the checker
         compiler = new Compiler();
         connect(compiler, SIGNAL(compilationFinished(const QString &)), this, SLOT(onCompilationFinished()));
         connect(compiler, SIGNAL(compilationErrorOccured(const QString &)), this,
                 SLOT(onCompilationErrorOccured(const QString &)));
         connect(compiler, SIGNAL(compilationKilled()), this, SLOT(onCompilationKilled()));
-        compiler->start(checkerPath, command, "C++");
+        compiler->start(checkerPath, compileCommand, "C++");
     }
 }
 
 void Checker::reqeustCheck(int index, const QString &input, const QString &output, const QString &expected)
 {
     if (compiled)
-        check(index, input, output, expected);
+        check(index, input, output, expected); // check immediately if the checker is compiled
     else
-        pendingTasks.push_back({index, input, output, expected});
+        pendingTasks.push_back({index, input, output, expected}); // otherwise push it into the pending tasks list
 }
 
 void Checker::onCompilationFinished()
 {
     Log::i("Checker/onCompilationFinished", "Invoked");
-    compiled = true;
+    compiled = true; // mark that the checker is compiled
     for (auto t : pendingTasks)
-        check(t.index, t.input, t.output, t.expected);
+        check(t.index, t.input, t.output, t.expected); // solve the pending tasks
     pendingTasks.clear();
 }
 
@@ -152,18 +161,23 @@ void Checker::onCompilationErrorOccured(const QString &error)
 
 void Checker::onCompilationKilled()
 {
+    // It will be confusing to show "the checker failed" when it's killed,
+    // but the user is also unlikely willing to see the message "the checker compilation is killed",
+    // so we can simply show nothing when the compilation is killed
     disconnect(compiler, SIGNAL(compilationErrorOccured(const QString &)), this,
                SLOT(onCompilationErrorOccured(const QString &)));
 }
 
-void Checker::onRunFinished(int index, const QString &out, const QString &err, int exitCode, int timeUsed)
+void Checker::onRunFinished(int index, const QString &out, const QString &err, int exitCode)
 {
     if (exitCode == 0)
     {
+        // the check process succeeded
         emit checkFinished(index, AC);
     }
-    else if (QList<int>({1, 2, 3, 4, 5, 8, 16}).contains(exitCode)) // from testlib.h::TResult
+    else if (QList<int>({1, 2, 3, 4, 5, 8, 16}).contains(exitCode)) // this list is from testlib.h::TResult
     {
+        // This exit code is a normal exit code of a testlib checker, means WA or something else
         if (err.isEmpty())
             log->error("Checker[" + QString::number(index + 1) + "]",
                        "Checker exited with exit code " + QString::number(exitCode));
@@ -173,6 +187,7 @@ void Checker::onRunFinished(int index, const QString &out, const QString &err, i
     }
     else
     {
+        // This exit code is not one of the normal exit codes of a testlib checker, maybe the checker crashed
         log->error("Checker[" + QString::number(index + 1) + "]",
                    "Checker exited with unknown exit code " + QString::number(exitCode));
         if (!err.isEmpty())
@@ -198,29 +213,44 @@ void Checker::onRunKilled(int index)
 bool Checker::checkIgnoreTrailingSpaces(const QString &output, const QString &expected)
 {
     Core::Log::i("Checker/checkIgnoreTrailingSpaces", "Invoked");
+
+    // first, replace \r\n and \r by \n
     auto out = output;
     out.replace("\r\n", "\n").replace("\r", "\n");
     auto ans = expected;
     ans.replace("\r\n", "\n").replace("\r", "\n");
-    auto a_lines = out.split('\n');
-    auto b_lines = ans.split('\n');
-    while (!a_lines.isEmpty() && a_lines.back().trimmed().isEmpty())
-        a_lines.pop_back();
-    while (!b_lines.isEmpty() && b_lines.back().trimmed().isEmpty())
-        b_lines.pop_back();
-    if (a_lines.size() != b_lines.size())
+
+    // split output and answer into lines
+    auto outputLines = out.split('\n');
+    auto answerLines = ans.split('\n');
+
+    // remove trailing empty lines
+    while (!outputLines.isEmpty() && outputLines.back().trimmed().isEmpty())
+        outputLines.pop_back();
+    while (!answerLines.isEmpty() && answerLines.back().trimmed().isEmpty())
+        answerLines.pop_back();
+
+    // if they are considered the same, they must have the same number of lines after removing trailing empty lines
+    if (outputLines.size() != answerLines.size())
         return false;
-    for (int i = 0; i < a_lines.size(); ++i)
+
+    for (int i = 0; i < outputLines.size(); ++i)
     {
-        auto a_line = a_lines[i];
-        auto b_line = b_lines[i];
-        while (!a_line.isEmpty() && a_line.back().isSpace())
-            a_line.chop(1);
-        while (!b_line.isEmpty() && b_line.back().isSpace())
-            b_line.chop(1);
-        if (a_line != b_line)
+        auto outputLine = outputLines[i];
+        auto answerLine = answerLines[i];
+
+        // remove trailing spaces at the end of the current line
+        while (!outputLine.isEmpty() && outputLine.back().isSpace())
+            outputLine.chop(1);
+        while (!answerLine.isEmpty() && answerLine.back().isSpace())
+            answerLine.chop(1);
+
+        // if they are considered the same, the current line should be exactly the same after removing trailing spaces
+        if (outputLine != answerLine)
             return false;
     }
+
+    // all tests are passed, this output is accepted
     return true;
 }
 
@@ -229,6 +259,7 @@ bool Checker::checkStrict(const QString &output, const QString &expected)
     Log::i("Checker/checkStrict", "Invoked");
     auto a = output;
     auto b = expected;
+    // replace \r\n and \r with \n, then directly compare them
     return a.replace("\r\n", "\n").replace("\r", "\n") == b.replace("\r\n", "\n").replace("\r", "\n");
 }
 
@@ -237,6 +268,7 @@ void Checker::check(int index, const QString &input, const QString &output, cons
     Log::i("Checker/check") << "Invoked. " << INFO_OF(index) << endl;
     switch (checkerType)
     {
+    // check directly if it's a built-in checker
     case IgnoreTrailingSpaces:
         emit checkFinished(index, checkIgnoreTrailingSpaces(output, expected) ? AC : WA);
         break;
@@ -244,6 +276,7 @@ void Checker::check(int index, const QString &input, const QString &output, cons
         emit checkFinished(index, checkStrict(output, expected) ? AC : WA);
         break;
     default:
+        // if it's a testlib checker, save the input, output and expected files first
         auto inputPath = tmpDir->filePath(QString::number(index) + ".in");
         auto outputPath = tmpDir->filePath(QString::number(index) + ".out");
         auto expectedPath = tmpDir->filePath(QString::number(index) + ".ans");
@@ -251,10 +284,11 @@ void Checker::check(int index, const QString &input, const QString &output, cons
             Util::saveFile(outputPath, output, "Checker", false, log) &&
             Util::saveFile(expectedPath, expected, "Checker", false, log))
         {
+            // if files are successfully saved, run the checker
             auto tmp = new Runner(index);
-            runner.push_back(tmp);
+            runner.push_back(tmp); // save the checkers in a list, so we can delete them when destructing the checker
             connect(tmp, SIGNAL(runFinished(int, const QString &, const QString &, int, int)), this,
-                    SLOT(onRunFinished(int, const QString &, const QString &, int, int)));
+                    SLOT(onRunFinished(int, const QString &, const QString &, int)));
             connect(tmp, SIGNAL(runErrorOccured(int, const QString &)), this,
                     SLOT(onRunErrorOccured(int, const QString &)));
             connect(tmp, SIGNAL(runTimeout(int)), this, SLOT(onRunTimeout(int)));
