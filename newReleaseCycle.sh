894f5a8ce0a7b6d6b746c06a3ecd3ce6cdfa908f
ouuan <y___o___u@126.com>
2020-03-08 13:10:34 +0800

    chore: Update release.sh and add newReleaseCycle.sh
    They haven't been tested yet.

---
+++ newReleaseCycle.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+if (($# != 8)); then
+  echo 'newReleaseCycle.sh <stable major> <stable minor> <stable patch> <beta major> <beta minor> <beta patch> <new major> <new minor>'
+  exit 1
+fi
+
+set -e
+
+export STABLEMINOR=v$1.$2
+export STABLEOLD=$1.$2.$3
+
+export BETAMINOR=v$4.$5
+export BETAOLD=$4.$5.$6
+export BETANEW=$4.$5.$(($6 + 1))
+
+export NEWMINOR=v$7.$8
+export NEWNEW=$7.$8.1
+
+export ALPHAOLD=$4.$(($5 + 1)).0
+export ALPHANEW=$7.$(($8 + 1)).0
+
+export NEXTMINOR=v$7.$(($8 + 1))
+
+cat << endOfMessage
+The old versions:
+Alpha:	master	$ALPHAOLD
+Beta:	$BETAMINOR	$BETAOLD
+Stable:	$STABLEMINOR	$STABLEOLD
+
+The new versions:
+Alpha:	master	$ALPHANEW
+Stable:	$BETAMINOR	$BETANEW
+Beta:	$NEWMINOR	$NEWNEW
+
+Continue?
+endOfMessage
+
+read
+
+git switch $BETAMINOR
+sed -i "s/UNRELEASED/UNRELEASED\n\n## $BETANEW/" doc/CHANGELOG.md
+sed -i "s/$BETAOLD/$BETANEW/" CMakeLists.txt
+git add doc/CHANGELOG.md CMakeLists.txt
+git diff --cached
+echo "Is this diff of $BETANEW OK?"
+read
+git commit -m "Release $BETANEW"
+git tag $BETANEW
+git push
+git push --tags
+
+git switch -c $NEWMINOR master
+git merge $BETAMINOR --no-commit
+echo "[$NEWMINOR]: Are the conflicts resolved?"
+read
+git commit -am "Merge $BETAMINOR: Release $BETANEW"
+sed -i "s/UNRELEASED/UNRELEASED\n\n## $NEWNEW/" doc/CHANGELOG.md
+sed -i "s/$ALPHAOLD/$NEWNEW/" CMakeLists.txt
+git add doc/CHANGELOG.md CMakeLists.txt
+git diff --cached
+echo "Is this diff of $NEWNEW OK?"
+read
+git commit -m "Release $NEWNEW"
+git tag $NEWNEW
+git push
+git push --tags
+
+git switch master
+git merge $NEWMINOR --no-commit
+echo "[master]: Are the conflicts resolved?"
+read
+git commit -am "Merge $NEWMINOR: Release $NEWNEW"
+sed -i "s/UNRELEASED/UNRELEASED\n\n## v$NEWMINOR/" doc/CHANGELOG.md
+sed -i "s/$ALPHAOLD/$ALPHANEW/" CMakeLists.txt
+sed -i "s/$BETAMINOR/$NEWMINOR/" README.md
+sed -i "s/$BETAOLD/$NEWNEW/" README.md
+sed -i "s/$STABLEMINOR/$BETAMINOR/" README.md
+sed -i "s/$STABLEOLD/$BETANEW/" README.md
+git add doc/CHANGELOG.md CMakeLists.txt README.md
+git diff --cached
+echo "Is this diff of the new release cycle OK?"
+read
+git commit -m "Start a new release cycle for $NEXTMINOR"
+git push
