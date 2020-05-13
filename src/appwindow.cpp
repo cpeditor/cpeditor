cpeditor/cpeditor

commit d5ea61ba4d6c5fe553c32d877ea1de14529883b2

feat: Load tabs from files after crash if hot exit is on
See 3fab159 for the old behavior.

Now, if hot exit is on, the tabs will be restored after a crash, and the
contents will be loaded from file instead of the saved status, because
of the reason mentioned in 3fab159.

src/appwindow.cpp
@@ -60,14 +60,20 @@ AppWindow::AppWindow(bool noHotExit, QWidget *parent) : QMainWindow(parent), ui(
         auto oldSize = size();
         setUpdatesEnabled(false);
 
+        // save these so that they won't be affected by saveEditorStatus() in onEditorChanged()
+        QVector<MainWindow::EditorStatus> status;
+        for (int i = 0; i < length; ++i)
+            status.push_back(settingManager->getEditorStatus(i));
+        bool loadFromFile = settingManager->isHotExitLoadFromFile();
+        int currentIndex = settingManager->getCurrentIndex();
+
         for (int i = 0; i < length; ++i)
         {
             if (progress.wasCanceled())
                 break;
-            auto status = MainWindow::EditorStatus(settingManager->getEditorStatus(i));
             progress.setValue(i);
             openTab("");
-            currentWindow()->loadStatus(status);
+            currentWindow()->loadStatus(status[i], loadFromFile);
             progress.setLabelText(currentWindow()->getTabTitle(true, false));
         }
 
@@ -76,11 +82,10 @@ AppWindow::AppWindow(bool noHotExit, QWidget *parent) : QMainWindow(parent), ui(
         setUpdatesEnabled(true);
         resize(oldSize);
 
-        settingManager->clearEditorStatus();
+        settingManager->setHotExitLoadFromFile(true);
 
-        int index = settingManager->getCurrentIndex();
-        if (index >= 0 && index < ui->tabWidget->count())
-            ui->tabWidget->setCurrentIndex(index);
+        if (currentIndex >= 0 && currentIndex < ui->tabWidget->count())
+            ui->tabWidget->setCurrentIndex(currentIndex);
     }
 }
 
@@ -438,25 +443,31 @@ void AppWindow::openContest(const QString &path, const QString &lang, int number
     openTabs(tabs);
 }
 
-bool AppWindow::quit()
+void AppWindow::saveEditorStatus(bool loadFromFile)
 {
     settingManager->clearEditorStatus();
+    if (ui->tabWidget->count() == 1 && windowIndex(0)->isUntitled() && !windowIndex(0)->isTextChanged())
+    {
+        settingManager->setNumberOfTabs(0);
+        settingManager->setCurrentIndex(-1);
+    }
+    else
+    {
+        settingManager->setNumberOfTabs(ui->tabWidget->count());
+        settingManager->setCurrentIndex(ui->tabWidget->currentIndex());
+        for (int i = 0; i < ui->tabWidget->count(); ++i)
+        {
+            settingManager->setEditorStatus(i, windowIndex(i)->toStatus(loadFromFile).toMap());
+        }
+    }
+}
+
+bool AppWindow::quit()
+{
     if (settingManager->isUseHotExit())
     {
-        if (ui->tabWidget->count() == 1 && windowIndex(0)->isUntitled() && !windowIndex(0)->isTextChanged())
-        {
-            settingManager->setNumberOfTabs(0);
-            settingManager->setCurrentIndex(-1);
-        }
-        else
-        {
-            settingManager->setNumberOfTabs(ui->tabWidget->count());
-            settingManager->setCurrentIndex(ui->tabWidget->currentIndex());
-            for (int i = 0; i < ui->tabWidget->count(); ++i)
-            {
-                settingManager->setEditorStatus(i, windowIndex(i)->toStatus().toMap());
-            }
-        }
+        settingManager->setHotExitLoadFromFile(false);
+        saveEditorStatus(false);
         return true;
     }
     else
@@ -711,6 +722,10 @@ void AppWindow::onTabChanged(int index)
 void AppWindow::onEditorChanged()
 {
     Core::Log::i("appwindow/onEditorChanged", "Invoked onEditorChanged");
+
+    if (settingManager->isUseHotExit() && settingManager->isHotExitLoadFromFile())
+        saveEditorStatus(true);
+
     if (currentWindow() != nullptr)
     {
         QMap<QString, QVector<int>> tabsByName;
@@ -764,7 +779,6 @@ void AppWindow::onSettingsApplied()
             connect(server, &Network::CompanionServer::onRequestArrived, this, &AppWindow::onIncomingCompanionRequest);
     diagonistics = true;
     onTabChanged(ui->tabWidget->currentIndex());
-    onEditorChanged();
 
     Core::Log::i("appwindow/onSettingsApplied", "Applied settings to appwindow");
 }

src/mainwindow.cpp
@@ -18,6 +18,7 @@
 #include "mainwindow.hpp"
 
 #include "Core/Compiler.hpp"
+#include "Core/EventLogger.hpp"
 #include "Core/MessageLogger.hpp"
 #include "Core/Runner.hpp"
 #include "Extensions/EditorTheme.hpp"
@@ -303,44 +304,57 @@ QMap<QString, QVariant> MainWindow::EditorStatus::toMap() const
 }
 #undef TOSTATUS
 
-MainWindow::EditorStatus MainWindow::toStatus() const
+MainWindow::EditorStatus MainWindow::toStatus(bool simple) const
 {
     EditorStatus status;
 
     status.isLanguageSet = isLanguageSet;
     status.filePath = filePath;
-    status.savedText = savedText;
     status.problemURL = problemURL;
-    status.editorText = editor->toPlainText();
     status.language = language;
-    status.editorCursor = editor->textCursor().position();
-    status.editorAnchor = editor->textCursor().anchor();
-    status.horizontalScrollBarValue = editor->horizontalScrollBar()->value();
-    status.verticalScrollbarValue = editor->verticalScrollBar()->value();
     status.untitledIndex = untitledIndex;
-    status.input = testcases->inputs();
-    status.expected = testcases->expecteds();
+
+    if (!simple)
+    {
+        status.editorText = editor->toPlainText();
+        status.editorCursor = editor->textCursor().position();
+        status.editorAnchor = editor->textCursor().anchor();
+        status.horizontalScrollBarValue = editor->horizontalScrollBar()->value();
+        status.verticalScrollbarValue = editor->verticalScrollBar()->value();
+        status.input = testcases->inputs();
+        status.expected = testcases->expecteds();
+    }
 
     return status;
 }
 
-void MainWindow::loadStatus(const EditorStatus &status)
+void MainWindow::loadStatus(const EditorStatus &status, bool simple)
 {
-    filePath = status.filePath;
-    updateWatcher();
-    savedText = status.savedText;
     setProblemURL(status.problemURL);
-    editor->setPlainText(status.editorText);
     if (status.isLanguageSet)
         setLanguage(status.language);
-    auto cursor = editor->textCursor();
-    cursor.setPosition(status.editorAnchor);
-    cursor.setPosition(status.editorCursor, QTextCursor::KeepAnchor);
-    editor->setTextCursor(cursor);
-    editor->horizontalScrollBar()->setValue(status.horizontalScrollBarValue);
-    editor->verticalScrollBar()->setValue(status.verticalScrollbarValue);
     untitledIndex = status.untitledIndex;
-    testcases->loadStatus(status.input, status.expected);
+
+    Core::Log::i("MainWindow/loadStatus") << INFO_OF(simple) << ' ' << INFO_OF(status.filePath) << endl;
+
+    if (!simple)
+    {
+        filePath = status.filePath;
+        updateWatcher();
+        savedText = status.savedText;
+        editor->setPlainText(status.editorText);
+        auto cursor = editor->textCursor();
+        cursor.setPosition(status.editorAnchor);
+        cursor.setPosition(status.editorCursor, QTextCursor::KeepAnchor);
+        editor->setTextCursor(cursor);
+        editor->horizontalScrollBar()->setValue(status.horizontalScrollBarValue);
+        editor->verticalScrollBar()->setValue(status.verticalScrollbarValue);
+        testcases->loadStatus(status.input, status.expected);
+    }
+    else if (!status.filePath.isEmpty())
+    {
+        loadFile(status.filePath);
+    }
 }
 
 void MainWindow::applyCompanion(Network::CompanionData data)

include/appwindow.hpp
@@ -163,6 +163,7 @@ class AppWindow : public QMainWindow
     void openPaths(const QStringList &paths, bool cpp = true, bool java = true, bool python = true, int depth = -1);
     QStringList openFolder(const QString &path, bool cpp, bool java, bool python, int depth);
     void openContest(const QString &path, const QString &lang, int number);
+    void saveEditorStatus(bool loadFromFile);
     bool quit();
 
     MainWindow *currentWindow();

include/mainwindow.hpp
@@ -76,8 +76,8 @@ class MainWindow : public QMainWindow
 
     void setProblemURL(const QString &url);
 
-    EditorStatus toStatus() const;
-    void loadStatus(const EditorStatus &status);
+    EditorStatus toStatus(bool simple) const;
+    void loadStatus(const EditorStatus &status, bool simple);
 
     void save(bool force, const QString &head);
     void saveAs();

include/Core/SettingsManager.hpp
@@ -160,6 +160,8 @@ class SettingManager
     void clearEditorStatus();
     QMap<QString, QVariant> getEditorStatus(int index);
     void setEditorStatus(int index, const QMap<QString, QVariant> &status);
+    bool isHotExitLoadFromFile();
+    void setHotExitLoadFromFile(bool value);
 
     QString getCFPath();
     void setCFPath(const QString &path);

src/Core/SettingsManager.cpp
@@ -444,7 +444,6 @@ void SettingManager::setCurrentIndex(int index)
 }
 void SettingManager::clearEditorStatus()
 {
-    setNumberOfTabs(0);
     mSettings->remove("editor_status");
 }
 QMap<QString, QVariant> SettingManager::getEditorStatus(int index)
@@ -454,6 +453,14 @@ QMap<QString, QVariant> SettingManager::getEditorStatus(int index)
 void SettingManager::setEditorStatus(int index, const QMap<QString, QVariant> &status)
 {
     mSettings->setValue("editor_status/" + QString::number(index), status);
+}
+bool SettingManager::isHotExitLoadFromFile()
+{
+    return mSettings->value("hot_exit_load_from_file", "false").toBool();
+}
+void SettingManager::setHotExitLoadFromFile(bool value)
+{
+    mSettings->setValue("hot_exit_load_from_file", value ? "true" : "false");
 }
 
 int SettingManager::getTransparency()

commit 1fb5d6bbd8e4768019e5a72691ac72ab051acc32

refactor: Remove saving tabs and loading from files when crashes
That feature was introduced by d5ea61ba4d6c5fe553c32d877ea1de14529883b2,
but it hasn't been working since the new preferences window, because now
the settings are saved on normal exit.

Also, after signals handling is finished, that feature will be less
useful, so we can simply remove it.

src/appwindow.cpp
@@ -119,21 +119,14 @@ AppWindow::AppWindow(bool noHotExit, QWidget *parent) : QMainWindow(parent), ui(
         auto oldSize = size();
         setUpdatesEnabled(false);
 
-        // save these so that they won't be affected by saveEditorStatus() in onEditorFileChanged()
-        QVector<MainWindow::EditorStatus> status;
-        for (int i = 0; i < length; ++i)
-            status.push_back(
-                MainWindow::EditorStatus(SettingsManager::get(QString("Editor Status/%1").arg(i)).toMap()));
-        bool loadFromFile = SettingsHelper::isHotExitLoadFromFile();
-        int currentIndex = SettingsHelper::getHotExitCurrentIndex();
-
         for (int i = 0; i < length; ++i)
         {
             if (progress.wasCanceled())
                 break;
+            auto status = MainWindow::EditorStatus(SettingsManager::get(QString("Editor Status/%1").arg(i)).toMap());
             progress.setValue(i);
             openTab("");
-            currentWindow()->loadStatus(status[i], loadFromFile);
+            currentWindow()->loadStatus(status);
             progress.setLabelText(currentWindow()->getTabTitle(true, false));
         }
 
@@ -142,8 +135,7 @@ AppWindow::AppWindow(bool noHotExit, QWidget *parent) : QMainWindow(parent), ui(
         setUpdatesEnabled(true);
         resize(oldSize);
 
-        SettingsHelper::setHotExitLoadFromFile(true);
-
+        int currentIndex = SettingsHelper::getHotExitCurrentIndex();
         if (currentIndex >= 0 && currentIndex < ui->tabWidget->count())
             ui->tabWidget->setCurrentIndex(currentIndex);
     } while (false);
@@ -533,9 +525,8 @@ void AppWindow::openContest(const QString &path, const QString &lang, int number
     openTabs(tabs);
 }
 
-void AppWindow::saveEditorStatus(bool loadFromFile)
+void AppWindow::saveEditorStatus()
 {
-    LOG_INFO("Save Editor status and load From File " << BOOL_INFO_OF(loadFromFile));
     SettingsManager::remove(SettingsManager::keyStartsWith("Editor Status/"));
     if (ui->tabWidget->count() == 1 && windowAt(0)->isUntitled() && !windowAt(0)->isTextChanged() &&
         windowAt(0)->getProblemURL().isEmpty())
@@ -548,7 +539,7 @@ void AppWindow::saveEditorStatus(bool loadFromFile)
         SettingsHelper::setHotExitTabCount(ui->tabWidget->count());
         SettingsHelper::setHotExitCurrentIndex(ui->tabWidget->currentIndex());
         for (int i = 0; i < ui->tabWidget->count(); ++i)
-            SettingsManager::set(QString("Editor Status/%1").arg(i), windowAt(i)->toStatus(loadFromFile).toMap());
+            SettingsManager::set(QString("Editor Status/%1").arg(i), windowAt(i)->toStatus().toMap());
     }
 }
 
@@ -559,8 +550,7 @@ bool AppWindow::quit()
     if (SettingsHelper::isHotExitEnable() || SettingsHelper::isForceClose())
     {
         LOG_INFO("quit() with hotexit");
-        SettingsHelper::setHotExitLoadFromFile(false);
-        saveEditorStatus(false);
+        saveEditorStatus();
     }
     else
     {
@@ -864,9 +854,6 @@ void AppWindow::onTabChanged(int index)
 
 void AppWindow::onEditorFileChanged()
 {
-    if (SettingsHelper::isHotExitEnable() && SettingsHelper::isHotExitLoadFromFile())
-        saveEditorStatus(true);
-
     if (currentWindow() != nullptr)
     {
         QMap<QString, QVector<int>> tabsByName;

src/appwindow.hpp
@@ -222,7 +222,7 @@ class AppWindow : public QMainWindow
     void openPaths(const QStringList &paths, bool cpp = true, bool java = true, bool python = true, int depth = -1);
     QStringList openFolder(const QString &path, bool cpp, bool java, bool python, int depth);
     void openContest(const QString &path, const QString &lang, int number);
-    void saveEditorStatus(bool loadFromFile);
+    void saveEditorStatus();
     bool quit();
     int getNewUntitledIndex();
     void reAttachLanguageServer(MainWindow *window);

src/mainwindow.cpp
@@ -368,9 +368,8 @@ QMap<QString, QVariant> MainWindow::EditorStatus::toMap() const
 }
 #undef TOSTATUS
 
-MainWindow::EditorStatus MainWindow::toStatus(bool simple) const
+MainWindow::EditorStatus MainWindow::toStatus() const
 {
-    LOG_INFO("Moving to status this window state" << BOOL_INFO_OF(simple));
     EditorStatus status;
 
     status.isLanguageSet = isLanguageSet;
@@ -380,24 +379,20 @@ MainWindow::EditorStatus MainWindow::toStatus(bool simple) const
     status.untitledIndex = untitledIndex;
     status.checkerIndex = testcases->checkerIndex();
     status.customCheckers = testcases->customCheckers();
-
-    if (!simple)
-    {
-        status.editorText = editor->toPlainText();
-        status.editorCursor = editor->textCursor().position();
-        status.editorAnchor = editor->textCursor().anchor();
-        status.horizontalScrollBarValue = editor->horizontalScrollBar()->value();
-        status.verticalScrollbarValue = editor->verticalScrollBar()->value();
-        status.input = testcases->inputs();
-        status.expected = testcases->expecteds();
-        for (int i = 0; i < testcases->count(); ++i)
-            status.testcasesIsShow.push_back(testcases->isShow(i));
-    }
+    status.editorText = editor->toPlainText();
+    status.editorCursor = editor->textCursor().position();
+    status.editorAnchor = editor->textCursor().anchor();
+    status.horizontalScrollBarValue = editor->horizontalScrollBar()->value();
+    status.verticalScrollbarValue = editor->verticalScrollBar()->value();
+    status.input = testcases->inputs();
+    status.expected = testcases->expecteds();
+    for (int i = 0; i < testcases->count(); ++i)
+        status.testcasesIsShow.push_back(testcases->isShow(i));
 
     return status;
 }
 
-void MainWindow::loadStatus(const EditorStatus &status, bool simple)
+void MainWindow::loadStatus(const EditorStatus &status)
 {
     LOG_INFO("Requesting loadStatus");
     setProblemURL(status.problemURL);
@@ -406,29 +401,19 @@ void MainWindow::loadStatus(const EditorStatus &status, bool simple)
     untitledIndex = status.untitledIndex;
     testcases->addCustomCheckers(status.customCheckers);
     testcases->setCheckerIndex(status.checkerIndex);
-
-    LOG_INFO(INFO_OF(simple) << INFO_OF(status.filePath));
-
-    if (!simple)
-    {
-        filePath = status.filePath;
-        updateWatcher();
-        savedText = status.savedText;
-        editor->setPlainText(status.editorText);
-        auto cursor = editor->textCursor();
-        cursor.setPosition(status.editorAnchor);
-        cursor.setPosition(status.editorCursor, QTextCursor::KeepAnchor);
-        editor->setTextCursor(cursor);
-        editor->horizontalScrollBar()->setValue(status.horizontalScrollBarValue);
-        editor->verticalScrollBar()->setValue(status.verticalScrollbarValue);
-        testcases->loadStatus(status.input, status.expected);
-        for (int i = 0; i < status.testcasesIsShow.count() && i < testcases->count(); ++i)
-            testcases->setShow(i, status.testcasesIsShow[i].toBool());
-    }
-    else if (!status.filePath.isEmpty())
-    {
-        loadFile(status.filePath);
-    }
+    filePath = status.filePath;
+    updateWatcher();
+    savedText = status.savedText;
+    editor->setPlainText(status.editorText);
+    auto cursor = editor->textCursor();
+    cursor.setPosition(status.editorAnchor);
+    cursor.setPosition(status.editorCursor, QTextCursor::KeepAnchor);
+    editor->setTextCursor(cursor);
+    editor->horizontalScrollBar()->setValue(status.horizontalScrollBarValue);
+    editor->verticalScrollBar()->setValue(status.verticalScrollbarValue);
+    testcases->loadStatus(status.input, status.expected);
+    for (int i = 0; i < status.testcasesIsShow.count() && i < testcases->count(); ++i)
+        testcases->setShow(i, status.testcasesIsShow[i].toBool());
 }
 
 void MainWindow::applyCompanion(const Extensions::CompanionData &data)

src/mainwindow.hpp
@@ -92,8 +92,8 @@ class MainWindow : public QMainWindow
     void setProblemURL(const QString &url);
     void setUntitledIndex(int index);
 
-    EditorStatus toStatus(bool simple) const;
-    void loadStatus(const EditorStatus &status, bool simple);
+    EditorStatus toStatus() const;
+    void loadStatus(const EditorStatus &status);
 
     void save(bool force, const QString &head, bool safe = true);
     void saveAs();