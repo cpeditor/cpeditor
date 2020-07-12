2c86b0fb237540d2f9b68a186eeeb336199185ea
Yufan You <ouuansteve@gmail.com>
2020-07-12 09:23:53 +0800

    tools: remove unnecessary arguments of updateTranslation.sh

--- tools/updateTranslation.sh
+++ tools/updateTranslation.sh
@@ -8,18 +8,8 @@ else
 	LOCATIONS=none
 fi
 
-declare -a SOURCES
-declare -a DIRS
-declare -a INCS
-
-SOURCES=(src/*.cpp src/*/*.cpp build/generated/*.cpp build/generated/*.hpp build/cpeditor_autogen/ui/*.h build/third_party/QtFindReplaceDialog/dialogs/QtFindReplaceDialog_autogen/include/*.h)
-DIRS=(Core Extensions Settings Telemetry Util Widgets)
-INCS=()
-
-for i in "${DIRS[@]}"; do
-	INCS=("${INCS[@]}" -I src/"$i");
-done
+SOURCES=(src build/generated build/cpeditor_autogen/ui build/third_party/QtFindReplaceDialog/dialogs/QtFindReplaceDialog_autogen)
 
 for i in zh_CN ru_RU; do
-	lupdate -no-obsolete "${SOURCES[@]}" -locations "$LOCATIONS" -tr-function-alias tr+=TRKEY -ts translations/$i.ts -I src "${INCS[@]}"
+	lupdate -no-obsolete "${SOURCES[@]}" -locations "$LOCATIONS" -tr-function-alias tr+=TRKEY -ts translations/$i.ts
 done
