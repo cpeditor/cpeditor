14426e15c32b36829471474a0e016c1d1480ff2e
Yufan You <ouuansteve@gmail.com>
2020-07-22 15:42:38 +0800

    fix: use %NUMBER_OF_PROCESSORS% on Windows

--- .ci/win/install_ecm.bat
+++ .ci/win/install_ecm.bat
@@ -5,5 +5,5 @@ mkdir build
 cd build
 cmake .. -DBUILD_HTML_DOCS=OFF -DBUILD_MAN_DOCS=OFF -DBUILD_QTHELP_DOCS=OFF -DBUILD_TESTING=OFF 
 
-cmake --build . --config Release -j $(nproc)
+cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --build . --target install
--- .ci/win/install_ksh.bat
+++ .ci/win/install_ksh.bat
@@ -7,5 +7,5 @@ mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_HTML_DOCS=OFF -DBUILD_MAN_DOCS=OFF -DBUILD_QTHELP_DOCS=OFF -DBUILD_TESTING=OFF 
 
-cmake --build . --config Release -j $(nproc)
+cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --build . --target install
