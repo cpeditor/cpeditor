805edb96f2769ca862c9861cc6551e5d531a2876
Ashar <coder3101@users.noreply.github.com>
2020-09-15 20:54:10 +0530

    Match GYM Urls
    GYM problems can be submitted through CF Tool. Now, CP Editor will parse them as well.

--- src/Extensions/CFTool.cpp
+++ src/Extensions/CFTool.cpp
@@ -139,6 +139,14 @@ bool CFTool::parseCfUrl(const QString &url, QString &contestId, QString &problem
         problemCode = match.captured(2);
         return true;
     }
+    
+    match = QRegularExpression(".*://codeforces.com/gym/([1-9][0-9]*)/problem/(0|[A-Z][1-9]?)").match(url);
+    if(match.hasMatch())
+    {
+            contestId = match.captured(1);
+            problemCode = match.captured(2);
+            return true;
+    }
     return false;
 }
 
