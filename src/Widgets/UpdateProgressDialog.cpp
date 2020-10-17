715f38a4c3ed8db68b186255b3342b3f77f554a0
Yufan You <ouuansteve@gmail.com>
2020-10-17 21:00:33 +0800

    feat(UpdateChecker): update download links

--- src/Widgets/UpdateProgressDialog.cpp
+++ src/Widgets/UpdateProgressDialog.cpp
@@ -62,7 +62,7 @@ void UpdateProgressDialog::onUpdateFailed(const QString &error)
 {
     information->setText(
         tr("Error: %1<br /><br />Updater failed to check for update. Please manually check for update at<br /><a "
-           "href=\"https://cpeditor.github.io/download\">https://cpeditor.github.io/download</a> or <a "
+           "href=\"https://cpeditor.org/download\">https://cpeditor.org/download</a> or <a "
            "href=\"https://github.com/cpeditor/cpeditor/releases\">https://github.com/cpeditor/cpeditor/releases</a>.")
             .arg(error));
     progressBar->hide();
