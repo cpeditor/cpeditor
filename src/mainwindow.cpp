cpeditor/cpeditor

commit 7eb8b3fd82d31508dba847dbb7bf28a1524a3634

feat(formatter): add YAPF and refactor (#652)
* feat(formatter): add YAPF and refactor

* docs(CHANGELOG): update changelog

* refactor: use constructor instead of new for formatters

* Add .clangd/ in ignore list

Co-authored-by: Ashar Khan <ashar786khan@gmail.com>
Co-authored-by: Ashar <coder3101@users.noreply.github.com>

src/mainwindow.cpp
@@ -25,6 +25,7 @@
 #include "Extensions/CFTool.hpp"
 #include "Extensions/ClangFormatter.hpp"
 #include "Extensions/CompanionServer.hpp"
+#include "Extensions/YAPFormatter.hpp"
 #include "Settings/DefaultPathManager.hpp"
 #include "Settings/FileProblemBinder.hpp"
 #include "Settings/PreferencesWindow.hpp"
@@ -67,9 +68,6 @@ MainWindow::MainWindow(int index, AppWindow *parent)
     log = new MessageLogger(appWindow->getPreferencesWindow(), this);
     ui->messageLoggerLayout->addWidget(log);
 
-    formatter = new Extensions::ClangFormatter(SettingsHelper::getClangFormatPath(),
-                                               SettingsHelper::getClangFormatStyle(), log);
-
     testcases = new Widgets::TestCases(log, this);
     ui->testCasesLayout->addWidget(testcases);
     connect(testcases, SIGNAL(checkerChanged()), this, SLOT(updateChecker()));
@@ -111,7 +109,6 @@ MainWindow::~MainWindow()
     delete ui;
     delete autoSaveTimer;
     delete testcases;
-    delete formatter;
     delete fileWatcher;
     delete editor;
     delete log;
@@ -590,12 +587,6 @@ void MainWindow::applySettings(const QString &pagePath)
         return pagePath.isEmpty() || pagePath == page;
     };
 
-    if (pageChanged("Extensions/Clang Format"))
-    {
-        formatter->updateBinary(SettingsHelper::getClangFormatPath());
-        formatter->updateStyle(SettingsHelper::getClangFormatStyle());
-    }
-
     if (pageChanged("Extensions/CF Tool"))
     {
         cftoolPath = SettingsHelper::getCFPath();
@@ -710,10 +701,13 @@ void MainWindow::compileAndRun()
     compile();
 }
 
-void MainWindow::formatSource()
+void MainWindow::formatSource(bool selectionOnly, bool logOnNoChange)
 {
     LOG_INFO("Requested code format");
-    formatter->format(editor, filePath, language, true, true);
+    if (language == "Python")
+        Extensions::YAPFormatter(editor, language, selectionOnly, logOnNoChange, log, this).format();
+    else
+        Extensions::ClangFormatter(editor, language, selectionOnly, logOnNoChange, log, this).format();
 }
 
 void MainWindow::setLanguage(const QString &lang)
@@ -935,9 +929,11 @@ bool MainWindow::saveFile(SaveMode mode, const QString &head, bool safe)
 {
     LOG_INFO(INFO_OF(mode) << INFO_OF(head) << BOOL_INFO_OF(safe));
 
-    if ((mode != AutoSave && SettingsHelper::isClangFormatFormatOnManualSave()) ||
-        (mode == AutoSave && SettingsHelper::isClangFormatFormatOnAutoSave()))
-        formatter->format(editor, filePath, language, false, false);
+    if ((mode != AutoSave && SettingsHelper::isFormatOnManualSave()) ||
+        (mode == AutoSave && SettingsHelper::isFormatOnAutoSave()))
+    {
+        formatSource(false, false);
+    }
 
     if (mode == SaveAs || (isUntitled() && mode == AlwaysSave))
     {
@@ -966,14 +962,11 @@ bool MainWindow::saveFile(SaveMode mode, const QString &head, bool safe)
                 }
             }
             if (defaultPath.isEmpty())
-                defaultPath =
-                    QDir(DefaultPathManager::defaultPathForAction("Save File")).filePath(getTabTitle(false, false));
-            if (language == "C++")
-                defaultPath += ".cpp";
-            else if (language == "Java")
-                defaultPath += ".java";
-            else if (language == "Python")
-                defaultPath += ".py";
+            {
+                defaultPath = Util::fileNameWithSuffix(
+                    QDir(DefaultPathManager::defaultPathForAction("Save File")).filePath(getTabTitle(false, false)),
+                    language);
+            }
         }
 
         // Create the parent directories of the default path if they don't exist,

.gitignore
@@ -44,6 +44,7 @@ target_wrapper.*
 CMakeLists.txt.user*
 
 build/
+.clangd/
 
 # Visual Studio
 .vs

CMakeLists.txt
@@ -126,6 +126,10 @@ add_executable(cpeditor
     src/Extensions/CFTool.hpp
     src/Extensions/ClangFormatter.cpp
     src/Extensions/ClangFormatter.hpp
+    src/Extensions/CodeFormatter.cpp
+    src/Extensions/CodeFormatter.hpp
+    src/Extensions/YAPFormatter.cpp
+    src/Extensions/YAPFormatter.hpp
     src/Extensions/CompanionServer.cpp
     src/Extensions/CompanionServer.hpp
     src/Extensions/EditorTheme.cpp

src/Extensions/ClangFormatter.cpp
@@ -16,215 +16,72 @@
  */
 
 #include "Extensions/ClangFormatter.hpp"
-#include "Core/EventLogger.hpp"
-#include "Core/MessageLogger.hpp"
-#include "Util/FileUtil.hpp"
-#include "generated/SettingsHelper.hpp"
 #include <QCodeEditor>
 #include <QJsonDocument>
-#include <QProcess>
-#include <QTemporaryDir>
+#include <QTextCursor>
 
 namespace Extensions
 {
-ClangFormatter::ClangFormatter(const QString &clangFormatBinary, const QString &clangFormatStyle, MessageLogger *logger,
-                               QObject *parent)
-    : QObject(parent)
+
+ClangFormatter::ClangFormatter(QCodeEditor *editor, const QString &lang, bool selectionOnly, bool logOnNoChange,
+                               MessageLogger *log, QObject *parent)
+    : CodeFormatter(editor, lang, selectionOnly, logOnNoChange, log, parent)
 {
-    log = logger;
-    updateBinary(clangFormatBinary);
-    updateStyle(clangFormatStyle);
 }
 
-bool ClangFormatter::check(const QString &checkBinary, const QString &checkStyle)
+QString ClangFormatter::settingKey() const
 {
-    LOG_INFO(INFO_OF(checkBinary));
-
-    // create a temporary directory for formatting
-    QTemporaryDir tmpDir;
-    if (!tmpDir.isValid())
-    {
-        LOG_WARN("tmpDir is not valid");
-        return false;
-    }
-
-    // save a simple file for testing
-    if (!Util::saveFile(tmpDir.filePath("tmp.cpp"), "int main(){}", tr("Formatter/check")))
-        return false;
-
-    // save the style to .clang-format
-    if (!Util::saveFile(tmpDir.filePath(".clang-format"), checkStyle, tr("Formatter/check")))
-        return false;
-
-    // run Clang Format
-    QProcess program;
-    program.start(checkBinary, {"--cursor=0", "--offset=0", "--length=0", "--style=file", tmpDir.filePath("tmp.cpp")});
-    LOG_INFO(INFO_OF(program.arguments().join(" ")));
-
-    // check whether the process finished in 2 seconds with exit code 0
-    if (!program.waitForFinished(2000))
-    {
-        LOG_WARN("Format process did not finished in 2 sec, it is being killed forcefully");
-        program.kill();
-        return false;
-    }
-    LOG_INFO(INFO_OF(program.exitCode()));
-    return program.exitCode() == 0;
+    return "Clang Format";
 }
 
-void ClangFormatter::updateBinary(const QString &newBinary)
+QStringList ClangFormatter::arguments() const
 {
-    LOG_INFO("Updated clangformat binary to " << newBinary);
-    binary = newBinary;
+    return {"--style=file", QString("--cursor=%1").arg(anchorPos)};
 }
 
-void ClangFormatter::updateStyle(const QString &newStyle)
+QStringList ClangFormatter::rangeArgs() const
 {
-    style = newStyle;
+    return {QString("--offset=%1").arg(qMin(cursorPos, anchorPos)),
+            QString("--length=%1").arg(qAbs(cursorPos - anchorPos))};
 }
 
-void ClangFormatter::format(QCodeEditor *editor, const QString &filePath, const QString &lang, bool selectionOnly,
-                            bool verbose)
+QString ClangFormatter::styleFileName() const
 {
-    LOG_INFO(BOOL_INFO_OF(editor == nullptr) << INFO_OF(filePath) << BOOL_INFO_OF(selectionOnly));
+    return ".clang-format";
+}
 
-    // get the cursor positions
+QString ClangFormatter::newSource(const QString &out) const
+{
+    return out.mid(out.indexOf('\n') + 1);
+}
+
+QTextCursor ClangFormatter::newCursor(const QString &out, const QStringList &args) const
+{
     auto cursor = editor->textCursor();
-    int start = cursor.selectionStart();
-    int end = cursor.selectionEnd();
-    int pos = cursor.position();
+    cursor.setPosition(newCursorPos(out));
 
-    // get command line arguments related to cursor position
-    QStringList args = {"--cursor=" + QString::number(pos), "--style=file"};
-    if (selectionOnly && cursor.hasSelection())
+    if (cursorPos != anchorPos)
     {
-        args.append("--offset=" + QString::number(start));
-        args.append("--length=" + QString::number(end - start));
+        // change the `--cursor` argument and format again to get the position of the other end of the selection
+
+        QStringList newArgs;
+
+        for (const auto &arg : qAsConst(args))
+            if (!arg.startsWith("--cursor"))
+                newArgs.append(arg);
+        newArgs.append(QString("--cursor=%1").arg(cursorPos));
+
+        auto res = runProcess(newArgs);
+        cursor.setPosition(newCursorPos(res), QTextCursor::KeepAnchor);
     }
 
-    // get the file name
-    QString tmpName = "tmp.cpp";
-    if (filePath.isEmpty())
-    {
-        // if the file path is empty, generate one depends on the language
-        if (lang == "Java")
-            tmpName = "tmp.java";
-        else if (lang == "Python")
-            tmpName = "tmp.py";
-    }
-    else
-    {
-        // otherwise, use the actual file name
-        tmpName = QFileInfo(filePath).fileName();
-    }
-
-    // create a temporary directory
-    QTemporaryDir tmpDir;
-    if (!tmpDir.isValid())
-    {
-        log->error(tr("Formatter"), tr("Failed to create temporary directory"));
-        return;
-    }
-
-    // save the text to a file
-    if (!Util::saveFile(tmpDir.filePath(tmpName), editor->toPlainText(), tr("Formatter"), true, log))
-        return;
-
-    // save the style to .clang-format
-    if (!Util::saveFile(tmpDir.filePath(".clang-format"), style, tr("Formatter"), true, log))
-        return;
-
-    // add the file to be formatted in the command line arguments
-    args.append(tmpDir.filePath(tmpName));
-
-    // get format result with the current cursor
-    auto res = getFormatResult(args);
-    if (res.first == -1)
-    {
-        return;
-    }
-
-    if (res.second == editor->toPlainText())
-    {
-        LOG_INFO("Already formatted");
-        if (verbose)
-            log->info(tr("Formatter"), tr("Formatting completed"));
-        return;
-    }
-
-    // set the formatted text and cursor position
-    cursor.select(QTextCursor::Document);
-    cursor.insertText(res.second);
-    cursor.setPosition(res.first);
-
-    if (start != end)
-    {
-        // if there's a selection, we have to restore not only the cursor, but also the anchor
-
-        // get the new position for the anchor
-        if (pos == end)
-            args[0] = "--cursor=" + QString::number(start);
-        else
-            args[0] = "--cursor=" + QString::number(end);
-        auto res2 = getFormatResult(args);
-        if (res2.first == -1)
-            return;
-
-        // restore the selection
-        cursor.setPosition(res2.first, QTextCursor::MoveAnchor);
-        cursor.setPosition(res.first, QTextCursor::KeepAnchor);
-    }
-
-    // apply the cursor changes to the editor
-    editor->setTextCursor(cursor);
-
-    log->info(tr("Formatter"), tr("Formatting completed"));
+    return cursor;
 }
 
-QPair<int, QString> ClangFormatter::getFormatResult(const QStringList &args)
+int ClangFormatter::newCursorPos(const QString &out) const
 {
-    // run the format porcess
-
-    QProcess formatProcess;
-    formatProcess.start(binary, args);
-    LOG_INFO("Starting format with args : " << args.join(","));
-
-    bool finished = formatProcess.waitForFinished(2000); // BLOCKS main Thread
-    if (!finished)
-    {
-        formatProcess.kill();
-        log->error(
-            tr("Formatter"),
-            tr("The format process didn't finish in 2 seconds. This is probably because the clang-format binary is not "
-               "found by CP Editor. You can set the path to clang-format at %1.")
-                .arg(SettingsHelper::pathOfClangFormatPath()),
-            false);
-        return QPair<int, QString>(-1, QString());
-    }
-
-    if (formatProcess.exitCode() != 0)
-    {
-        // the format process failed, show the error messages and return {-1, QString()}
-        LOG_WARN("Format process returned exit code " << formatProcess.exitCode());
-
-        log->warn(tr("Formatter"), tr("The format command is: %1 %2").arg(binary, args.join(' ')));
-        auto stdOut = formatProcess.readAllStandardOutput();
-        if (!stdOut.isEmpty())
-            log->warn(tr("Formatter[stdout]"), stdOut);
-        auto stdError = formatProcess.readAllStandardError();
-        if (!stdError.isEmpty())
-            log->error(tr("Formatter[stderr]"), stdError);
-        return QPair<int, QString>(-1, QString());
-    }
-
-    // get the formatted codes and the new cursor position
-    auto result = formatProcess.readAllStandardOutput();
-    int firstNewLine = result.indexOf('\n');
-    QString jsonLine = result.left(firstNewLine);
-    auto formattedCodes = result.mid(firstNewLine + 1);
-    auto json = QJsonDocument::fromJson(jsonLine.toLocal8Bit());
-    int newCursorPos = json["Cursor"].toInt();
-
-    return QPair<int, QString>(newCursorPos, formattedCodes);
+    auto json = QJsonDocument::fromJson(out.left(out.indexOf('\n')).toUtf8());
+    return json["Cursor"].toInt();
 }
+
 } // namespace Extensions

src/Extensions/CodeFormatter.cpp
@@ -1,0 +1,153 @@
+/*
+ * Copyright (C) 2019-2020 Ashar Khan <ashar786khan@gmail.com>
+ *
+ * This file is part of CP Editor.
+ *
+ * CP Editor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * I will not be responsible if CP Editor behaves in unexpected way and
+ * causes your ratings to go down and or lose any important contest.
+ *
+ * Believe Software is "Software" and it isn't immune to bugs.
+ *
+ */
+
+#include "Extensions/CodeFormatter.hpp"
+#include "Core/EventLogger.hpp"
+#include "Core/MessageLogger.hpp"
+#include "Settings/SettingsManager.hpp"
+#include "Util/FileUtil.hpp"
+#include <QCodeEditor>
+#include <QProcess>
+#include <QTemporaryDir>
+
+namespace Extensions
+{
+
+CodeFormatter::CodeFormatter(QCodeEditor *editor, const QString &lang, bool selectionOnly, bool logOnNoChange,
+                             MessageLogger *log, QObject *parent)
+    : QObject(parent), editor(editor), lang(lang), selectionOnly(selectionOnly), logOnNoChange(logOnNoChange), log(log)
+{
+    LOG_INFO(INFO_OF(lang) << INFO_OF(selectionOnly) << INFO_OF(logOnNoChange));
+
+    auto cursor = editor->textCursor();
+    cursorPos = cursor.position();
+    cursorLine = cursor.blockNumber();
+    cursorCol = cursor.columnNumber();
+    anchorPos = cursor.anchor();
+    cursor.setPosition(anchorPos);
+    anchorLine = cursor.blockNumber();
+    anchorCol = cursor.columnNumber();
+
+    LOG_INFO(INFO_OF(cursorPos) << INFO_OF(cursorLine) << INFO_OF(anchorPos) << INFO_OF(anchorLine));
+}
+
+void CodeFormatter::format() const
+{
+    QStringList args = arguments() << QProcess::splitCommand(getSetting("Arguments").toString());
+
+    if (formatSelectionOnly())
+        args.append(rangeArgs());
+
+    QTemporaryDir tmpDir;
+    if (!tmpDir.isValid())
+    {
+        log->error(tr("Formatter"), tr("Failed to create temporary directory"));
+        return;
+    }
+
+    auto tmpPath = tmpDir.filePath(Util::fileNameWithSuffix("tmp", lang));
+    args.append(tmpPath);
+
+    if (!Util::saveFile(tmpPath, editor->toPlainText(), tr("Formatter"), true, log))
+        return;
+
+    if (!Util::saveFile(tmpDir.filePath(styleFileName()), getSetting("Style").toString(), tr("Formatter"), true, log))
+        return;
+
+    auto res = runProcess(args);
+    if (res.isEmpty())
+        return;
+
+    auto source = newSource(res);
+
+    if (source == editor->toPlainText())
+    {
+        if (logOnNoChange)
+            log->info(tr("Formatter"), tr("Formatting completed"));
+        return;
+    }
+
+    auto cursor = editor->textCursor();
+    cursor.select(QTextCursor::Document);
+    cursor.insertText(source);
+
+    editor->setTextCursor(newCursor(res, args));
+
+    log->info(tr("Formatter"), tr("Formatting completed"));
+}
+
+QString CodeFormatter::runProcess(const QStringList &args) const
+{
+    QProcess formatProcess;
+    formatProcess.start(getSetting("Program").toString(), args);
+    LOG_INFO(INFO_OF(formatProcess.program()) << INFO_OF(formatProcess.arguments().join(' ')));
+
+    bool finished = formatProcess.waitForFinished(2000); // blocks main Thread
+    if (!finished)
+    {
+        formatProcess.kill();
+        log->error(tr("Formatter"),
+                   tr("The format process didn't finish in 2 seconds. This is probably because the %1 program is not "
+                      "found by CP Editor. You can set the path to the program at %2.")
+                       .arg(settingKey())
+                       .arg(SettingsManager::getPathText(settingKey() + "/Program")),
+                   false);
+        return QString();
+    }
+
+    auto exitCode = formatProcess.exitCode();
+
+    if (exitCode != 0)
+    {
+        LOG_WARN(INFO_OF(exitCode));
+
+        log->warn(tr("Formatter"), tr("The format command [%1 %2] finished with exit code %3.")
+                                       .arg(formatProcess.program())
+                                       .arg(formatProcess.arguments().join(' '))
+                                       .arg(exitCode));
+        auto stdOut = formatProcess.readAllStandardOutput();
+        if (!stdOut.isEmpty())
+            log->warn(tr("Formatter[stdout]"), stdOut);
+        auto stdError = formatProcess.readAllStandardError();
+        if (!stdError.isEmpty())
+            log->error(tr("Formatter[stderr]"), stdError);
+        return QString();
+    }
+
+    auto out = formatProcess.readAllStandardOutput();
+
+    if (out.isEmpty())
+    {
+        LOG_WARN("Output is empty");
+        log->warn(tr("Formatter"), tr("The output of the format process is empty. Please ensure there is no in-place "
+                                      "modification option in the formatting arguments."));
+    }
+
+    return out;
+}
+
+bool CodeFormatter::formatSelectionOnly() const
+{
+    return selectionOnly && cursorPos != anchorPos;
+}
+
+QVariant CodeFormatter::getSetting(const QString &key) const
+{
+    return SettingsManager::get(settingKey() + "/" + key);
+}
+
+} // namespace Extensions

src/Extensions/CodeFormatter.hpp
@@ -1,0 +1,105 @@
+/*
+ * Copyright (C) 2019-2020 Ashar Khan <ashar786khan@gmail.com>
+ *
+ * This file is part of CP Editor.
+ *
+ * CP Editor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * I will not be responsible if CP Editor behaves in unexpected way and
+ * causes your ratings to go down and or lose any important contest.
+ *
+ * Believe Software is "Software" and it isn't immune to bugs.
+ *
+ */
+
+#ifndef CODEFORMATTER_HPP
+#define CODEFORMATTER_HPP
+
+#include <QObject>
+
+class MessageLogger;
+class QCodeEditor;
+class QTextCursor;
+
+namespace Extensions
+{
+class CodeFormatter : public QObject
+{
+    Q_OBJECT
+
+  public:
+    explicit CodeFormatter(QCodeEditor *editor, const QString &lang, bool selectionOnly, bool logOnNoChange,
+                           MessageLogger *log, QObject *parent = nullptr);
+
+    void format() const;
+
+  protected:
+    /**
+     * @brief the key of the formatter in the settings, e.g. Clang Format
+     * @note there should be these settings: settingKey()/Program, settingKey()/Arguments, settingKey()/Style
+     */
+    virtual QString settingKey() const = 0;
+
+    /**
+     * @brief the arguments of the formatter, except args for formatting a range and args set by the user
+     */
+    virtual QStringList arguments() const = 0;
+
+    /**
+     * @brief the arguments used to format selection only
+     * @note you can use cursorPos, cursorLine, cursorCol, anchorPos, anchorLine, anchorCol in this function
+     */
+    virtual QStringList rangeArgs() const = 0;
+
+    /**
+     * @brief the name of the style file, e.g. .clang-format
+     */
+    virtual QString styleFileName() const = 0;
+
+    /**
+     * @brief the new source code after formatting
+     * @param out the stdout of the format process
+     */
+    virtual QString newSource(const QString &out) const = 0;
+
+    /**
+     * @brief the new text cursor after formatting
+     * @param out the stdout of the format process
+     * @param args the arguments used when formatting
+     */
+    virtual QTextCursor newCursor(const QString &out, const QStringList &args) const = 0;
+
+    /**
+     * @brief run the format process and get the result
+     * @param args the arguments of the format process
+     * @returns the stdout of the format process, or an empty QString if error occurred
+     */
+    QString runProcess(const QStringList &args) const;
+
+    /**
+     * @brief check whether only the selection is formatted
+     */
+    bool formatSelectionOnly() const;
+
+  private:
+    /**
+     * @brief get settingKey()/key
+     */
+    QVariant getSetting(const QString &key) const;
+
+  protected:
+    QCodeEditor *editor;
+    QString lang;
+    bool selectionOnly;
+    bool logOnNoChange;
+    int cursorPos, cursorLine, cursorCol, anchorPos, anchorLine, anchorCol;
+
+  private:
+    MessageLogger *log = nullptr;
+};
+} // namespace Extensions
+
+#endif // CODEFORMATTER_HPP

src/Extensions/YAPFormatter.cpp
@@ -1,0 +1,89 @@
+/*
+ * Copyright (C) 2019-2020 Ashar Khan <ashar786khan@gmail.com>
+ *
+ * This file is part of CP Editor.
+ *
+ * CP Editor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * I will not be responsible if CP Editor behaves in unexpected way and
+ * causes your ratings to go down and or lose any important contest.
+ *
+ * Believe Software is "Software" and it isn't immune to bugs.
+ *
+ */
+
+#include "Extensions/YAPFormatter.hpp"
+#include <QCodeEditor>
+#include <QTextBlock>
+#include <QTextCursor>
+#include <QTextDocument>
+
+namespace Extensions
+{
+
+YAPFormatter::YAPFormatter(QCodeEditor *editor, const QString &lang, bool selectionOnly, bool logOnNoChange,
+                           MessageLogger *log, QObject *parent)
+    : CodeFormatter(editor, lang, selectionOnly, logOnNoChange, log, parent)
+{
+}
+
+QString YAPFormatter::settingKey() const
+{
+    return "YAPF";
+}
+
+QStringList YAPFormatter::arguments() const
+{
+    return {};
+}
+
+QStringList YAPFormatter::rangeArgs() const
+{
+    return {QString("-l %1-%2").arg(qMin(cursorLine, anchorLine) + 1).arg(qMax(cursorLine, anchorLine) + 1)};
+}
+
+QString YAPFormatter::styleFileName() const
+{
+    return ".style.yapf";
+}
+
+QString YAPFormatter::newSource(const QString &out) const
+{
+    return out;
+}
+
+QTextCursor YAPFormatter::newCursor(const QString &out, const QStringList &) const
+{
+    auto cursor = editor->textCursor();
+
+    auto setCol = [this, &cursor](QTextCursor::MoveMode mode, int col) {
+        cursor.movePosition(QTextCursor::NextCharacter, mode,
+                            qMin(col, editor->document()->findBlockByNumber(cursor.blockNumber()).length() - 1));
+    };
+
+    cursor.movePosition(QTextCursor::Start);
+    cursor.movePosition(QTextCursor::NextBlock, QTextCursor::MoveAnchor, anchorLine);
+    setCol(QTextCursor::MoveAnchor, anchorCol);
+
+    if (cursorPos == anchorPos)
+        return cursor;
+
+    if (anchorLine <= cursorLine)
+    {
+        cursor.movePosition(QTextCursor::NextBlock, QTextCursor::KeepAnchor, cursorLine - anchorLine);
+    }
+    else
+    {
+        cursor.movePosition(QTextCursor::PreviousBlock, QTextCursor::KeepAnchor, anchorLine - cursorLine);
+        cursor.movePosition(QTextCursor::StartOfBlock, QTextCursor::KeepAnchor);
+    }
+
+    setCol(QTextCursor::KeepAnchor, cursorCol);
+
+    return cursor;
+}
+
+} // namespace Extensions

src/Extensions/YAPFormatter.hpp
@@ -15,26 +15,20 @@
  *
  */
 
-/*
- * The Formatter is used to format codes.
- * It runs synchronously, so the event loop is blocked while formatting.
- * The time limit for formatting is 2 seconds.
- */
-
-#ifndef FORMATTER_HPP
-#define FORMATTER_HPP
+#ifndef YAPFORMATTER_HPP
+#define YAPFORMATTER_HPP
 
 #include "Extensions/CodeFormatter.hpp"
 
 namespace Extensions
 {
-class ClangFormatter : public CodeFormatter
+class YAPFormatter : public CodeFormatter
 {
     Q_OBJECT
 
   public:
-    explicit ClangFormatter(QCodeEditor *editor, const QString &lang, bool selectionOnly, bool logOnNoChange,
-                            MessageLogger *log, QObject *parent = nullptr);
+    explicit YAPFormatter(QCodeEditor *editor, const QString &lang, bool selectionOnly, bool logOnNoChange,
+                          MessageLogger *log, QObject *parent = nullptr);
 
   protected:
     virtual QString settingKey() const override;
@@ -47,12 +41,9 @@ class ClangFormatter : public CodeFormatter
 
     virtual QString newSource(const QString &out) const override;
 
-    virtual QTextCursor newCursor(const QString &out, const QStringList &args) const override;
-
-  private:
-    int newCursorPos(const QString &out) const;
+    virtual QTextCursor newCursor(const QString &out, const QStringList &) const override;
 };
 
 } // namespace Extensions
 
-#endif // FORMATTER_HPP
+#endif // YAPFORMATTER_HPP

commit d8cf2be9594e9d3ee7307885810eaf5e2846315f

fix: add suffix for Default File Path For Problem URLs
This was introduced in #652.

src/mainwindow.cpp
@@ -37,15 +37,11 @@
 #include "generated/version.hpp"
 #include <QCodeEditor>
 #include <QFileSystemWatcher>
-#include <QFontDialog>
 #include <QInputDialog>
 #include <QMessageBox>
 #include <QMimeData>
 #include <QRegularExpression>
-#include <QSaveFile>
 #include <QScrollBar>
-#include <QShortcut>
-#include <QSyntaxStyle>
 #include <QTemporaryDir>
 #include <QTextBlock>
 #include <QTimer>
@@ -945,12 +941,14 @@ bool MainWindow::saveFile(SaveMode mode, const QString &head, bool safe)
                     }
                 }
             }
+
             if (defaultPath.isEmpty())
             {
-                defaultPath = Util::fileNameWithSuffix(
-                    QDir(DefaultPathManager::defaultPathForAction("Save File")).filePath(getTabTitle(false, false)),
-                    language);
+                defaultPath =
+                    QDir(DefaultPathManager::defaultPathForAction("Save File")).filePath(getTabTitle(false, false));
             }
+
+            defaultPath = Util::fileNameWithSuffix(defaultPath, language);
         }
 
         // Create the parent directories of the default path if they don't exist,