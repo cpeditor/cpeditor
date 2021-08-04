4bcdf0f3027195a09b67ae165877ddb050520ef1
nekosu <liao.hy@outlook.com>
2021-08-04 20:20:43 +0800

    Update SettingsInfo
    Add SettingIter for setting tracking with keys for object.
    Move some functions' implement to SettingsInfo.cpp
    Add calling method support.
    Add string formatting(replace @ to keys) support.
    Reconstruct SettingInfo.
    Add methods and replace onApply.
    Remove `old` support.

--- src/Settings/SettingsInfo.hpp
+++ src/Settings/SettingsInfo.hpp
@@ -22,56 +22,183 @@
 #include <QVariant>
 #include <functional>
 
-class ValueWidget;
-
 class SettingsInfo
 {
     Q_DECLARE_TR_FUNCTIONS(SettingsInfo)
 
   public:
+    class SettingInfo;
+
+    struct SettingIter
+    {
+        const SettingInfo *info;
+        QStringList pre,
+            trPre; // pre contains object key and name. trPre contains object key and desc. untrDesc is not used.
+
+        SettingIter() = default;
+        SettingIter(const SettingInfo *i) : info(i)
+        {
+        }
+        SettingIter &child(const QString &key, const QString &name)
+        {
+            return child(key, key, name);
+        }
+        SettingIter &child(const QString &key, const SettingInfo *ch)
+        {
+            return child(key, key, ch);
+        }
+        SettingIter &child(const QString &key, const QString &trKey, const QString &name);
+        SettingIter &child(const QString &key, const QString &trKey, const SettingInfo *ch);
+        QList<SettingIter> allVisibleChild(const QString &key) const;
+        QString key() const
+        {
+            auto p = pre;
+            p.append(info->name);
+            return p.join("/");
+        }
+        const SettingInfo *operator->() const
+        {
+            return info;
+        }
+        QString format(bool needtr, const QString &fmt) const
+        {
+            return needtr ? format(fmt) : formatuntr(fmt);
+        }
+        QString format(const QString &fmt) const
+        {
+            return _format(fmt, trPre, true);
+        }
+        QString formatuntr(const QString &fmt) const
+        {
+            return _format(fmt, pre, false);
+        }
+        QString getDesc(bool needtr = true) const
+        {
+            if (!info)
+                return "";
+            return format(needtr, needtr ? info->desc : info->untrDesc);
+        }
+        QVariant getDefault() const
+        {
+            if (!info)
+                return QVariant();
+            if (info->methods.contains("getDefault"))
+                return info->call("getDefault", "pre", pre, "param", getParam(), "rawparam", info->param, "def",
+                                  info->def);
+            return info->def;
+        }
+        QVariant getParam() const
+        {
+            if (!info)
+                return QVariant();
+            if (info->methods.contains("getParam"))
+                return info->call("getParam", "pre", pre, "param", info->param);
+            return info->param;
+        }
+        QString getTip(bool needtr = true) const
+        {
+            if (!info)
+                return "";
+            if (info->methods.contains("getTip"))
+                return format(needtr, info->call("getTip", "pre", pre, "param", getParam(), "rawparam", info->param,
+                                                 "needtr", needtr, "tip", needtr ? info->tip : info->untrTip)
+                                          .toString());
+            return needtr ? format(info->tip) : formatuntr(info->untrTip);
+        }
+        QVariant buildChildDefault(const QString &key) const;
+
+      private:
+        QString _format(const QString &fmt, const QStringList &p, bool needtr) const;
+    };
+
     class SettingInfo
     {
       public:
-        QString name;             // key
-        QString desc;             // translated description
-        QString untrDesc;         // untranslated description
-        QString type;             // int, bool, QString, etc.
-        QString ui;               // type of the widget
-        QString tip;              // translated tooltips
-        QString untrTip;          // untranslated tooltips
-        QString docAnchor;        // the anchor of the documentation
-        bool requireAllDepends{}; // false for one of the depends, true for all depends
-        bool immediatelyApply{};
-        std::function<void(SettingInfo *, ValueWidget *, QWidget *)> onApply;
+        QString name;           // key
+        QString desc;           // translated description
+        QString untrDesc;       // untranslated description
+        QString type;           // int, bool, QString, etc.
+        QString ui;             // type of the widget
+        QString tip;            // translated tooltips
+        QString untrTip;        // untranslated tooltips
+        QString docAnchor;      // the anchor of the documentation
+        bool requireAllDepends; // false for one of the depends, true for all depends
+        bool immediatelyApply;
+        QMap<QString, std::function<QVariant(QVariant)>> methods;
         QList<QPair<QString, std::function<bool(const QVariant &)>>> depends;
-        QList<QString> old; // the old keys of this setting
         QVariant def;
         QVariant param;
         QList<SettingInfo> child;
 
-        QString key() const
+        QString key() const // now only use for backward compability
         {
             return name.toLower().replace("c++", "cpp").replace(' ', '_');
+        }
+        int findChild(const QString &name) const;
+        template <typename... Extra> QVariant call(const QString &name, const Extra &... extra) const
+        {
+            if (!methods.contains(name))
+            {
+                return QVariant();
+            }
+            QMap<QString, QVariant> arg;
+            return _call(methods[name], arg, extra...);
+        }
+
+      private:
+        template <typename... Extra>
+        QVariant _call(std::function<QVariant(QVariant)> func, QMap<QString, QVariant> &arg, const QString &key,
+                       const QVariant &value, const Extra &... extra) const
+        {
+            arg[key] = value;
+            return _call(func, arg, extra...);
+        }
+        QVariant _call(std::function<QVariant(QVariant)> func, QMap<QString, QVariant> &arg, const QString &key,
+                       const QVariant &value) const
+        {
+            arg[key] = value;
+            return func(arg);
+        }
+        QVariant _call(std::function<QVariant(QVariant)> func, QMap<QString, QVariant> &arg) const
+        {
+            return func(arg);
         }
     };
 
     static void updateSettingInfo();
 
-    static SettingInfo findSetting(const QString &name, const QList<SettingInfo> &infos = settings)
+    static const SettingInfo &findSetting(const QString &name)
     {
+        return findSetting(name, getSettings());
+    }
+
+    static const SettingInfo &findSetting(const QString &name, const QList<SettingInfo> &infos)
+    {
+        static SettingInfo empty;
         for (const SettingInfo &si : infos)
             if (si.name == name)
                 return si;
-        return SettingInfo();
+        return empty;
     }
 
-    static QList<SettingInfo> getSettings()
+    static const QList<SettingInfo> &getSettings()
     {
-        return settings;
+        return fakeRoot.child;
+    }
+
+    static SettingIter beginIter(const QString &name)
+    {
+        return SettingIter(&findSetting(name, getSettings()));
+    }
+
+    static SettingIter fakeRootIter()
+    { // This fake root has no type. Only used for it's child.
+        return SettingIter(&fakeRoot);
     }
 
   private:
-    static QList<SettingInfo> settings;
+    static SettingInfo fakeRoot;
+    static QList<SettingInfo> &settings;
 
     friend class SettingsUpdater;
 };
