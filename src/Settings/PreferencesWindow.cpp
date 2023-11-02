c19829af9d5fa5ce5a63914c77a8ca5b94a4589b
Bruno Ricardo <brunomatheus2607@gmail.com>
2023-11-02 17:39:23 -0300

    feat: Add menu option to choose message logger colors
    Add two new settings: "Error Message Color" and "Warn Message Color".
    These settings are located at Appeareance -> General, and have a
    combo box to select the colors, using default Qt Colors names
    (see https://doc.qt.io/qt-5/stylesheet-reference.html#color).
    
    Addresses the issue #521

--- src/Settings/PreferencesWindow.cpp
+++ src/Settings/PreferencesWindow.cpp
@@ -232,7 +232,8 @@ PreferencesWindow::PreferencesWindow(QWidget *parent) : QMainWindow(parent)
         .end()
         .dir(TRKEY("Appearance"))
             .page(TRKEY("General"),{"Locale", "UI Style", "Editor Theme", "Opacity", "Test Case Maximum Height",
-                                    "Show Compile And Run Only", "Display EOLN In Diff", "Extra Bottom Margin"})
+                                    "Show Compile And Run Only", "Display EOLN In Diff", "Extra Bottom Margin",
+                                    "Error Message Color", "Warn Message Color"})
             .page(TRKEY("Font"), {"Show Only Monospaced Font", "Editor Font", "Test Cases Font", "Message Logger Font",
                                   "Use Custom Application Font", "Custom Application Font"})
         .end()
