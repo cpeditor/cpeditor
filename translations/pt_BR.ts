62b42279946fad276b5ab24b93d26adf4e3a6afc
Yufan You <ouuansteve@gmail.com>
2024-02-20 18:52:19 +0800

    chore: update translations

--- translations/pt_BR.ts
+++ translations/pt_BR.ts
@@ -2689,10 +2689,6 @@ This may reduce distractions caused by stopwatch updates.</source>
 Isso pode reduzir distrações causadas pelas atualizações do cronômetro.</translation>
     </message>
     <message>
-        <source>Highlight Error Line</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Highlight lines containing diagnostics</source>
         <translation type="unfinished"></translation>
     </message>
